c1eb90d599ce733d89858dec7cc87d9626b178e4
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-23 14:29:56 -0300

    Criando um FOR infinito e parando com um BREAK

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var soma int = 1
-
-	for soma < 100 {
-		soma += soma
-		fmt.Printf("Soma atual: %d \n", soma)
+	for {
+		fmt.Println("LOOP INFINITO")
+		break
 	}
 }
