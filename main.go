f921f726f87e799ef6c0a5bea06e5d9adc801382
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-23 14:14:51 -0300

    Condições IF e ELSE

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ func main() {
 	const ano_fundacao = 2004
 	var num_funcionarios int = 24000
 	var media_salarial float64 = 1800.50
-	trabalho_remoto := true
+	trabalho_remoto := false
+
+	if trabalho_remoto == true {
+		fmt.Printf(" O trabalho na %s é remoto! \n", nome_empresa)
+	} else {
+		fmt.Printf(" O trabalho na %s não é remoto! \n", nome_empresa)
+	}
 
 	fmt.Printf(" Empresa: %s \n Fundado em: %d \n Número de Funcionários: %d \n Média Salarial: %.2f \n Oferece remoto? %t", nome_empresa, ano_fundacao, num_funcionarios, media_salarial, trabalho_remoto)
 }
