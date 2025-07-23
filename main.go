3a7dbdc953c06cc41db36db34c0bc5e250c75f27
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-23 14:18:18 -0300

    Declaração de Inicialização, ou seja, nome so existe dentro da condição IF

--- main.go
+++ main.go
@@ -3,17 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	nome_empresa := "Ambev"
-	const ano_fundacao = 2004
-	var num_funcionarios int = 24000
-	var media_salarial float64 = 1800.50
-	trabalho_remoto := false
-
-	if trabalho_remoto == true {
-		fmt.Printf(" O trabalho na %s é remoto! \n", nome_empresa)
+	if nome := "Gustavo"; nome == "Gustavo"{
+		fmt.Println("Nome correto")
 	} else {
-		fmt.Printf(" O trabalho na %s não é remoto! \n", nome_empresa)
+		fmt.Println("Nome Incorreto")
 	}
-
-	fmt.Printf(" Empresa: %s \n Fundado em: %d \n Número de Funcionários: %d \n Média Salarial: %.2f \n Oferece remoto? %t", nome_empresa, ano_fundacao, num_funcionarios, media_salarial, trabalho_remoto)
 }
