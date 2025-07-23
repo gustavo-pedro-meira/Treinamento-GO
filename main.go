65f580daa3a77fd935fff5965459b7142c7591ce
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-23 14:01:37 -0300

    Imprimir valores com variaveis constantes, definidas para a empresa que sonho trabalhar

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	const nome = "Gustavo"
-	const sobrenome = "Pedro"
-	
-	fmt.Println(nome, sobrenome)
+	nome_empresa := "Ambev"
+	const ano_fundacao = 2004
+	var num_funcionarios int = 24000
+	var media_salarial float64 = 1800.50
+	trabalho_remoto := true
+
+	fmt.Printf(" Empresa: %s \n Fundado em: %d \n Número de Funcionários: %d \n Média Salarial: %.2f \n Oferece remoto? %t", nome_empresa, ano_fundacao, num_funcionarios, media_salarial, trabalho_remoto)
 }
