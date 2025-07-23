c6c6d389d0e7b7fdde77af512ca7ce023c86b03b
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-23 14:21:57 -0300

    Declaração de Inicialização, definindo duas

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	if nome := "Gustavo"; nome == "Gustavo"{
-		fmt.Println("Nome correto")
+	if nome, idade := "Gustavo", 18; nome == "Gustavo"{
+		fmt.Printf("Nome correto, idade: %d", idade)
 	} else {
 		fmt.Println("Nome Incorreto")
 	}
