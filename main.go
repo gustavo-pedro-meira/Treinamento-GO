85ec8ff2fc0ff8e40703a20a3ba94a32604fba11
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-24 13:23:22 -0300

    Struct mais ou menos uma classe bem basica

--- main.go
+++ main.go
@@ -3,18 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	notaAlunos := make(map[string]float64)
-
-	notaAlunos["Gustavo"] = 9.9
-	notaAlunos["Renan"] = 8.7
-
-	fmt.Printf("A nota de Gustavo é %.1f \n", notaAlunos["Gustavo"])
-	fmt.Println(notaAlunos)
-
-	nota, existe := notaAlunos["Gustavo"]
-	if existe {
-		fmt.Println("A nota Existe", nota)
-	} else {
-		fmt.Println("A nota não Existe")
+	type Usuario struct {
+		ID int
+		Nome string
+		Email string
+		Ativo bool
 	}
 }
