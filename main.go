b72710991998991da8aa97b0c7218ef0c0dd5911
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-24 13:30:41 -0300

    Struct com instanciamento e definindo valores

--- main.go
+++ main.go
@@ -9,4 +9,15 @@ func main() {
 		Email string
 		Ativo bool
 	}
+
+	gustavo_user := Usuario {
+		ID: 1,
+		Nome: "Gustavo",
+		Email: "gustavo16pedro@gmail.com",
+		Ativo: true,
+	}
+
+	fmt.Println("Nome: ", gustavo_user.Nome)
+	gustavo_user.Ativo = false
+	fmt.Println("Esta Ativo? ", gustavo_user.Ativo)
 }
