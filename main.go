a8a8efb0b447c516d7ca0ad1b94a1ab38db57fa5
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-24 16:07:01 -0300

    Código conta Bancaria

--- main.go
+++ main.go
@@ -2,47 +2,43 @@ package main
 
 import "fmt"
 
-type Produto struct {
-	Nome string
-	Preco float64
-	Quantidade int
+type ContaBancaria struct {
+	Titular string
+	Saldo float64
 }
 
-func (p *Produto) AplicarDesconto(percentual float64) {
-	desconto := p.Preco * (percentual / 100)
-	valor_desconto := p.Preco - desconto
-	fmt.Printf("Valor do Produto com Desconto: %.2f \n", valor_desconto)
-}
-
-func (p *Produto) ValorTotalEstoque() {
-	valor_total := p.Preco * float64(p.Quantidade)
-	fmt.Println("Valor total em estoque: ", valor_total)
-}
-
-func (p *Produto) AdicionarEstoque(quantidade int) {
-	p.Quantidade += quantidade
-	fmt.Printf("Quantidade de %d aumentada para o produto %s \n", p.Quantidade, p.Nome)
-}
-
-func(p *Produto) VenderProduto(quantidade int) {
-	if quantidade > p.Quantidade {
-		fmt.Println("Estoque Insuficiente")
+func (conta *ContaBancaria) Depositar(valor float64) {
+	if valor < 0.0 {
+		fmt.Println("Valor negativo \n")
 	} else {
-		p.Quantidade -= quantidade
-		fmt.Println("Venda Realizada")
+		conta.Saldo += valor
+		fmt.Printf("Valor depositado: %.2f \n", valor)
 	}
+}
+
+func (conta *ContaBancaria) Sacar(valor float64) {
+	if valor > conta.Saldo {
+		fmt.Println("Saldo Insuficiente")
+	} else {
+		conta.Saldo -= valor
+		fmt.Printf("Valor Sacado: %.2f \n", valor)
+	}
+}
+
+func (conta *ContaBancaria) VerSaldo() {
+	fmt.Printf("Saldo Disponivel: %.2f \n", conta.Saldo)
 }
 
 func main() {
-	notebook := Produto {
-		Nome: "Samsung Book",
-		Preco: 5000.00,
-		Quantidade: 5,
+	conta := ContaBancaria {
+		Titular: "Gustavo",
+		Saldo: 100.00,
 	}
 
-	notebook.ValorTotalEstoque()
-	notebook.AplicarDesconto(10)
-	notebook.AdicionarEstoque(2)
-	notebook.VenderProduto(2)
+	conta.Depositar(250)
+	conta.VerSaldo()
+	conta.Sacar(400.99)
+	conta.Sacar(300.27)
+	conta.VerSaldo()
 }
 
