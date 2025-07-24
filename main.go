db5fca881a9e26ab4f48497a4645f2683f7a55a0
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-24 09:09:02 -0300

    MAPA que ver se nota de Gustavo existe

--- main.go
+++ main.go
@@ -10,4 +10,11 @@ func main() {
 
 	fmt.Printf("A nota de Gustavo é %.1f \n", notaAlunos["Gustavo"])
 	fmt.Println(notaAlunos)
+
+	nota, existe := notaAlunos["Gustavo"]
+	if existe {
+		fmt.Println("A nota Existe", nota)
+	} else {
+		fmt.Println("A nota não Existe")
+	}
 }
