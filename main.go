5e579b50f58f5b799a9e5bc985c89ae95ed063de
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-07-28 14:19:43 -0300

    Codigo de Exercicio para treinamento

--- main.go
+++ main.go
@@ -2,49 +2,62 @@ package main
 
 import "fmt"
 
-type Vendavel interface {
-	ObterPreco() float64
+// Interface
+type Funcionario interface {
+	CalcularSalario() float64
 }
 
-type ProdutoFisico struct {
+// Classes
+type Desenvolvedor struct {
 	Nome string
-	Preco float64
+	SalarioBase float64
+	HorasExtras int
+	SalarioFinal float64
 }
 
-type Assinatura struct {
+type Gerente struct {
 	Nome string
-	PrecoMensal float64
-	Meses int
+	SalarioBase float64
+	Bonus float64
+	SalarioFinal float64
 }
 
-func(produto *ProdutoFisico) ObterPreco() float64 {
-	return produto.Preco
+// Função
+func (d *Desenvolvedor) CalcularSalario() float64 {
+	const ValorHoraExtra = 75.50
+	d.SalarioFinal = d.SalarioBase + (float64(d.HorasExtras) * ValorHoraExtra)
+	return d.SalarioFinal
 }
 
-func (assinatura *Assinatura) ObterPreco() float64 {
-	return assinatura.PrecoMensal * float64(assinatura.Meses)
+func (g *Gerente) CalcularSalario() float64 {
+	g.SalarioFinal = g.SalarioBase + g.Bonus
+	return g.SalarioFinal
 }
 
-func CalcularPreco (carrinho []Vendavel) {
-	total := 0.0
-	for _, item := range carrinho {
-		total += item.ObterPreco()
-		fmt.Printf("Adicionando item ao carrinho: R$ %.2f\n", item.ObterPreco())
+func CalcularCustoFolha(folha []Funcionario) float64 {
+	custoTotal := 0.0
+	for _, funcionario := range folha {
+		custoTotal += float64(funcionario.CalcularSalario())
 	}
-	fmt.Printf("O total do carrinho é R$ %.2f\n", total)
+	return custoTotal
 }
 
 func main() {
-	notebook := ProdutoFisico {
-		Nome: "Notebook",
-		Preco: 3500.00,
+	dev := Desenvolvedor {
+		Nome: "Gustavo",
+		SalarioBase: 3000.00,
+		HorasExtras: 4,
 	}
-	curso := Assinatura {
-		Nome: "Curso GOLAND",
-		PrecoMensal: 12.99,
-		Meses: 2,
+	ger := Gerente {
+		Nome: "Renan",
+		SalarioBase: 4000.50,
+		Bonus: 1200.12,
 	}
 
-	meuCarrinho := []Vendavel{&notebook, &curso}
-	CalcularPreco(meuCarrinho)
+	fmt.Println(dev.CalcularSalario())
+	fmt.Println(ger.CalcularSalario())
+
+	calculo := []Funcionario{&dev, &ger}
+
+	fmt.Printf("Folha Total: %.2f", CalcularCustoFolha(calculo))
 }
