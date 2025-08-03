86f066a191dec9aa0655f5c690e8b329f5518e11
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-03 16:33:42 -0300

    Criando as Rotas

--- router/router.go
+++ router/router.go
@@ -1,5 +1,31 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+	
+	v1.GET("/opening", func(c *gin.Context){
+		c.JSON(http.StatusOK, "GET Opening")
+	})
+
+	v1.POST("/opening", func(c *gin.Context){
+		c.JSON(http.StatusCreated, "POST Opening")
+	})
+
+	v1.PUT("/opening", func(c *gin.Context){
+		c.JSON(http.StatusOK, "PUT Opening")
+	})
+
+	v1.DELETE("/opening", func(c *gin.Context){
+		c.JSON(http.StatusOK, "DELETE Opening")
+	})
+
+	v1.GET("/openings", func(c *gin.Context){
+		c.JSON(http.StatusOK, "GET Openings")
+	})
+}
