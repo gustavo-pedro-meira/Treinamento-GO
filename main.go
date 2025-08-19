427c804ec004c29347bfd7843a9d1aa542e35dd2
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-19 15:06:09 -0300

    Criação do Package Config

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"github.com/gustavo-pedro-meira/Treinamento-GO/config"
 	"github.com/gustavo-pedro-meira/Treinamento-GO/router"
 )
 
 func main() {
+	// Inicializa as Config
+	err := config.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	// Inicializa as Rotas
 	router.Initialize()
 }
