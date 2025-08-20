a379c5d60548c0bc7583450beedd8754f32904e2
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-20 14:11:50 -0300

    Nova API

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import (
-	"github.com/gustavo-pedro-meira/Treinamento-GO/config"
-	"github.com/gustavo-pedro-meira/Treinamento-GO/router"
-)
 
 func main() {
-	// Inicializa as Config
-	err := config.Init()
-	if err != nil {
-		panic(err)
-	}
-
-	// Inicializa as Rotas
-	router.Initialize()
+	
 }
